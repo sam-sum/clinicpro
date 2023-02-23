de73895400c27204e537e7670ea6642af1e3e599
ycheun29 <ycheun29@my.centennialcollege.ca>
2023-02-23 18:08:41 -0500

    Updated search page

--- lib/views/search.dart
+++ lib/views/search.dart
@@ -1,64 +1,178 @@
+import 'package:clinicpro/views/patient_details.dart';
 import 'package:flutter/material.dart';
 import 'package:clinicpro/utilities/styles.dart';
-import 'package:clinicpro/views/patient_details.dart';
 import 'package:clinicpro/models/patient_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import '../providers/Patients.dart';
 
-class Search extends StatelessWidget {
+class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
 
   @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  var _isInit = true;
+  var _isLoading = false;
+
+  List<Patient> patientList = [];
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      Provider.of<Patients>(context).fetchAllPatients().then((_) {
+        setState(() {
+          _isLoading = false;
+        });
+      });
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    //final patientsData = Provider.of<Patients>(context);
+    patientList = Provider.of<Patients>(context).patients;
+
     return Material(
-      color: Styles.backgroundColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text('Search'),
-          const SizedBox(
-            height: 30,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // When the user taps the button,
-              // navigate to a named route and
-              // provide the arguments as a
-              // parameter.
-              // Navigator.pushNamed(
-              //   context,
-              //   PatientDetails.routeName,
-              //   arguments: Patient(
-              //       '1',
-              //       'XYZ962438',
-              //       'Peter',
-              //       'Parker',
-              //       '',
-              //       '',
-              //       '',
-              //       150,
-              //       80,
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       '',
-              //       ''),
-              // );
-            },
-            child: const Text(
-                'This is to simulate a click on a patient list item'),
-          ),
-        ],
-      ),
-    );
+        color: Styles.backgroundColor,
+        child: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: patientList.length,
+                          itemBuilder: ((context, index) {
+                            var item = patientList[index];
+                            return InkWell(
+                              onTap: () {
+                                Navigator.pushNamed(
+                                    context, PatientDetails.routeName,
+                                    arguments: item);
+                              },
+                              child: Card(
+                                elevation: 4,
+                                child: Padding(
+                                  padding: EdgeInsets.all(15),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Expanded(
+                                            flex: 3,
+                                            child: Padding(
+                                              padding:
+                                                  const EdgeInsets.all(5.0),
+                                              child: ClipRRect(
+                                                borderRadius:
+                                                    BorderRadius.circular(5.0),
+                                                child: Image.network(
+                                                    item.photoUrl!,
+                                                    errorBuilder: (context,
+                                                        exception, stackTrace) {
+                                                  return Container();
+                                                }),
+                                              ),
+                                            ),
+                                          ),
+                                          Expanded(
+                                            flex: 6,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  item.firstName! +
+                                                      ' ' +
+                                                      item.lastName!,
+                                                ),
+                                                Text(
+                                                  'Gender : ' + item.gender!,
+                                                ),
+                                                Row(
+                                                  mainAxisSize:
+                                                      MainAxisSize.min,
+                                                  children: [
+                                                    FaIcon(
+                                                      FontAwesomeIcons
+                                                          .addressCard,
+                                                      size: 15,
+                                                    ),
+                                                    Text(
+                                                      ' #' + item.idCardNumber!,
+                                                    ),
+                                                  ],
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                          Expanded(
+                                            flex: 4,
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Container(),
+                                                item.disabled == 'true'
+                                                    ? FaIcon(
+                                                        FontAwesomeIcons
+                                                            .wheelchair,
+                                                        size: 15,
+                                                      )
+                                                    : Container(),
+                                                Row(
+                                                  mainAxisSize:
+                                                      MainAxisSize.min,
+                                                  children: [
+                                                    FaIcon(
+                                                      FontAwesomeIcons.bed,
+                                                      size: 15,
+                                                    ),
+                                                    Text(
+                                                      ' ' +
+                                                          item.bedNumber!
+                                                              .toUpperCase(),
+                                                    ),
+                                                  ],
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                          Expanded(
+                                            flex: 1,
+                                            child: FaIcon(
+                                              FontAwesomeIcons.ellipsisVertical,
+                                              size: 15,
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          })),
+                    ),
+                  ],
+                ),
+              ));
   }
 }
